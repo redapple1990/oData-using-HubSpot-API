bccece940a0470b5f23194ed8be6898693fcd233
Kesavan <kesavan.keshav@gmail.com>
2021-03-05 14:23:11 +0530

    DataWrapper for get order changes

--- ODataWrapper/DataWrapper.cs
+++ ODataWrapper/DataWrapper.cs
@@ -1,13 +1,21 @@
-﻿using System;
-
+﻿using Reference;
+using System;
+using System.Collections.Generic;
 
 namespace ODataWrapper
 {
     public class DataWrapper
     {
-        public void GetCurrentContext()
+        public void GetOrderInfo()
         {
-            
+            var serviceRoot = "https://services.odata.org/V3/Northwind/Northwind.svc";
+            var context = new NorthwindEntities(new Uri(serviceRoot));
+
+            IEnumerable<Order> orders = context.Orders;
+            foreach (var o in orders)
+            {
+                Console.WriteLine("{0} {1}", o.OrderID, o.ShipRegion);
+            }
         }
     }
 }
