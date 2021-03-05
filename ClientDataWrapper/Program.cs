fc6a37d764cff9a067996f160d5b3bb60878bc92
Kesavan <kesavan.keshav@gmail.com>
2021-03-05 14:27:33 +0530

    Client Data Wrapper changes

---
+++ ClientDataWrapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClientDataWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var dataWrapper = new ODataWrapper.DataWrapper();
+            dataWrapper.GetOrderInfo();
+        }
+    }
+}
