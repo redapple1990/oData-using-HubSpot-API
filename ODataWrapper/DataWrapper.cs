9c53f1552d22ad3bee61b09ae81ffc6b2268ae39
Kesavan <kesavan.keshav@gmail.com>
2021-03-05 16:11:47 +0530

    Added OrderByProcessID

--- ODataWrapper/DataWrapper.cs
+++ ODataWrapper/DataWrapper.cs
@@ -1,33 +1,51 @@
 ﻿using Reference;
 using System;
 using System.Collections.Generic;
+using System.Data.Services.Client;
 
 namespace ODataWrapper
 {
     public class DataWrapper
     {
+        const string serviceRoot = "https://services.odata.org/V3/Northwind/Northwind.svc";
+
         public void GetOrderInfo()
         {
-            var serviceRoot = "https://services.odata.org/V3/Northwind/Northwind.svc";
             var context = new NorthwindEntities(new Uri(serviceRoot));
 
-            var test = context.Orders;
-            IAsyncResult resultOrder = test.BeginExecute((res) =>
+            var queryOrder = context.Orders;
+
+            foreach (var o in ProcessOrder(queryOrder))
+            {
+                string customer = (o.Customer == null) ?
+                                    "" : o.Customer.ContactName;
+                Console.WriteLine("Order ID : {0} for the customer is {1}", o.OrderID, customer);
+            }
+        }
+
+        public void GetOrderResponseByOrderID(string OrderID)
+        {
+            var context = new NorthwindEntities(new Uri(serviceRoot));
+            var queryOrder = context.Orders.AddQueryOption("Order ID", OrderID);
+
+            foreach (var o in ProcessOrder(queryOrder))
+            {
+                string customer = (o.Customer == null) ?
+                                    "" : o.Customer.ContactName;
+                Console.WriteLine("Order ID : {0} for the customer is {1}", o.OrderID, customer);
+            }
+        }
+
+        private IEnumerable<Order> ProcessOrder(DataServiceQuery<Order> queryOrder)
+        {
+            IAsyncResult resultOrder = queryOrder.BeginExecute((res) =>
             {
                 Console.WriteLine(res);
             }, null);
 
-            IEnumerable<Order> order = test.EndExecute(resultOrder);
+            IEnumerable<Order> objOrder = queryOrder.EndExecute(resultOrder);
 
-            foreach(var o in order)
-            {
-                Console.WriteLine(o.CustomerID);
-            }
-        }
-
-        public void GetOrderResponse()
-        {
-
+            return objOrder;
         }
     }
 }
